55360d6352889be9def216d847fcd4bbbb115236
YawarRaza7349 <YawarRaza7349@gmail.com>
2015-02-05 18:14:46 -0500

    Intital Xcode project

---
+++ ExchangeWithFriends/ExchangeWithFriends/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExchangeWithFriends
+//
+//  Created by Student on 2/5/15.
+//  Copyright (c) 2015 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
