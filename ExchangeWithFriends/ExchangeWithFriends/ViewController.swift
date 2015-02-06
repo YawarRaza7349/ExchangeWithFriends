0a5879a63ce9df05367659ea5b27fa9d6be733f1
YawarRaza7349 <YawarRaza7349@gmail.com>
2015-02-05 19:49:39 -0500

    Added a button to an empty screen...

--- ExchangeWithFriends/ExchangeWithFriends/ViewController.swift
+++ ExchangeWithFriends/ExchangeWithFriends/ViewController.swift
@@ -2,8 +2,8 @@
 //  ViewController.swift
 //  ExchangeWithFriends
 //
-//  Created by Student on 2/5/15.
-//  Copyright (c) 2015 Student. All rights reserved.
+//  Created by FriendsExchangingCode on 2/5/15.
+//  Copyright (c) 2015 FriendsExchangingCode. All rights reserved.
 //
 
 import UIKit
